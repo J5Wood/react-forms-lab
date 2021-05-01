afc85213005b333bfcbce04f60d44d229f260ec2
Jeremy Wood <js.wood206@gmail.com>
2021-04-30 22:12:32 -0700

    commit tree f6ac2bf3a505a778c690ce1e93a31782eb3ab577

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,36 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ''
+    };
+  }
+
+  handleKeyEntry = (e) => {
+    this.setState({
+      message: e.target.value
+    })
+  }
+
+  remainingChars() {
+    let chars = this.state.message.length
+    return (this.props.maxChars - chars)
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input 
+          type="text"
+          name="message"
+          id="message"
+          value={this.state.message}
+          onChange={this.handleKeyEntry}
+        />
+        <br/>
+        RemainingCharacters:
+        {this.remainingChars()}
       </div>
     );
   }
